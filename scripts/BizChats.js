f74a01beeb0608e9b2538275a2abd342271b5177
dennis19814 <dennis19814@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 07:55:04 +0000

    New Release files
    git-svn-id: https://plugins.svn.wordpress.org/free-live-chat-support/tags/1.2@1165533 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scripts/BizChats.js
@@ -0,0 +1,8 @@
+var BizChatId = BizChat_widget_object.BizChatId;
+var WidgetId = BizChat_widget_object.WidgetId;
+
+(function () {   
+	var script = document.createElement('script');
+	script.src = biz.biz_chat_plugin_path;
+	document.getElementsByTagName('HEAD').item(0).appendChild(script);    
+})();
